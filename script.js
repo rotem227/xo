8fb26c9b16b4e788d6be3eb4d5e8ea4f5d0e88c2
Rotem Ben Itzhak <rotem227@gmail.com>
2017-06-30 00:56:54 +0300

    starting with winning testings - horizintal test

--- script.js
+++ script.js
@@ -1,54 +1,134 @@
+var log = function(msg) {
+    console.log(msg);
+};
+
 var xo = {
     grid: 3,
+    board: [],
     cell: document.getElementsByClassName("xo-cell"),
-    symbol: (function() {
+    currentSumbol: "",
+    generateSymbol: (function() {
         var symbols = ["X", "O"],
             randomBinary = Math.floor(Math.random() * 2),
             chosenSymbol = symbols[randomBinary];
         
-        function replaceSymbol() {
+        function getSymbol() {
             return ( chosenSymbol == symbols[0] ) ? symbols[1] : symbols[0];
         }
         
-        console.log("FIRST SYMBOL: " + replaceSymbol());       
+        this.currentSymbol = getSymbol();
+        log("FIRST SYMBOL: " + this.currentSymbol);       
        
         return function() {
-            chosenSymbol = replaceSymbol();
+            chosenSymbol = getSymbol();
+            this.currentSymbol = chosenSymbol;
             return chosenSymbol;
         };
     }()),
-    setSymbol: function(clickedElement, index) {
+    createBoardArrays: function() {
+        var i = 0;
+        
+        for ( i = 0 ; i < this.grid ; i++ ) {
+            this.board.push({});
+            this.board[i]["group"+i+""] = [];
+        }
+        
+        log(this.board);
+    },
+    declareWinner: function(symbol = "") {
+        log("WIN!!! -> " + symbol + " is the winner!!!");
+    },
+    checkHorizontal: function() {
+        var cell,
+            rowNumber = 0,
+            cellNumber = 0,
+            winRow = -1,
+            searchInRow = true,
+            continueChecking = true;
+        
+        log("*********************************************");
+        log("Checking Horizonal:");
+        
+        while ( continueChecking ) {
+            for ( rowNumber = 0 ; rowNumber < this.grid ; rowNumber++ ) {
+                log("-------------------------------");
+                cell = this.board[rowNumber]["group"+rowNumber+""];
+                while ( searchInRow && cellNumber < this.grid ) {
+                    log("Checking in row number: " + rowNumber + " | searchInRow: " + searchInRow + " cellNumber: " + cellNumber);
+                    log("cell1 value: " + cell[cellNumber]);
+                    log("cell2 value: " + cell[cellNumber - 1]);
+                    if ( typeof(cell[cellNumber]) != "undefined" && typeof(cell[cellNumber - 1]) != "undefined" ) {
+                        if ( cell[cellNumber] != cell[cellNumber - 1] ) {
+                            searchInRow = false;    
+                            log("Different cells were found");
+                        }
+                    } else {
+                        searchInRow = false; 
+                        log("Empty cell was found");
+                    }
+                    cellNumber++;
+                }
+                if ( searchInRow ) {
+                    winRow = rowNumber;
+                    continueChecking = false;
+                    log("winRow was found: " + winRow);
+                } else {
+                    cellNumber = 0;
+                    searchInRow = true;
+                }
+                log("End of row number: " + rowNumber);
+                log("-------------------------------");
+            }
+        }
+        
+        log("*********************************************");
+        log("winRow: " + winRow);
+        return winRow;
+        
+    },
+    checkVertical: function() {
+    
+    },
+    checkDiagonal: function() {
+    
+    },
+    checkForWin: function() {
+        if ( this.checkHorizontal() > -1 ) {
+            this.declareWinner();
+        }
+        this.checkVertical();
+        this.checkDiagonal();
+    },
+    addSymbolToBoardGroup: function(index, symbol) {
+        var boardGroupNumber = Math.floor(index / this.grid);
+        this.board[boardGroupNumber]["group"+boardGroupNumber+""].push(symbol);
+        
+        this.checkForWin();
+    },
+    setSymbol: function(index, clickedElement) {
+        this.generateSymbol();
         if ( clickedElement.innerHTML == "" ) {
-            console.log("index: " + index);
-            clickedElement.innerHTML = this.symbol();
+            clickedElement.innerHTML = this.currentSymbol;
+            this.addSymbolToBoardGroup(index, this.currentSymbol);
         }
     },
-    addCellClickEvent: function(index) {
-        var THIS = this;
-        
-        console.log("index: " + index);
-        this.cell[index].addEventListener("click", function() {
-            THIS.setSymbol(this, index);
-        });
-    },
-    func: function(index) {
-        console.log(index);
-    },
-    init: function() {
+    createCellsClickEvents: function() {
         var i = 0,
             THIS = this;
-
-        for ( i = 0 ; i < xo.cell.length ; i++ ) {
-            //this.addCellClickEvent(i); 
-
-            var f = function() {
-                console.log();
-                //THIS.func(i);
-            };
             
-
-            this.cell[i].addEventListener("click", f);
+        function addClickEventToCell(index) {
+            THIS.cell[index].addEventListener("click", function() {
+                THIS.setSymbol(index, this);
+            });
         }
+        
+        for ( i = 0 ; i < xo.cell.length ; i++ ) {
+            addClickEventToCell(i); 
+        }
+    },
+    init: function() {
+        this.createBoardArrays();
+        this.createCellsClickEvents();
     }
 };
 
